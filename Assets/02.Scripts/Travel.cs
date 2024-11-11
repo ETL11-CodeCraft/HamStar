1b173c5c9c50f34050275b0c2a32b93589c61250
Seobok <Seobok@users.noreply.github.com>
2024-11-11 16:36:33 +0900

    여행 아이콘 클릭 시 남은 시간 출력
    테스트용 TravelScene제작
    버튼을 클릭하면 남은시간을 계산해 텍스트로 출력
    코루틴을 이용하여 3초후 텍스트가 자동으로 꺼지도록 설정
    여러번 버튼을 클릭하는데에 대한 예외처리

--- Assets/02.Scripts/Travel.cs
+++ Assets/02.Scripts/Travel.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.XR;
 
 public class Travel : MonoBehaviour
 {
+    [SerializeField] private GameObject travelIcon;
+    [SerializeField] private TextMeshProUGUI remainTravelText;
     string travelStartTime;
+    DateTime startTime;
     string travelEndTime;
+    DateTime endTime;
     bool isTraveling = false;
+    Coroutine HideCoroutine;
 
     private void Start()
     {
@@ -25,9 +32,12 @@ public class Travel : MonoBehaviour
         RefreshTravel();
     }
 
+    /// <summary>
+    /// 남은 시간을 새로고침하여 햄스터의 출발, 도착을 정하는 함수
+    /// </summary>
     public void RefreshTravel()
     {
-        DateTime endTime = DateTime.ParseExact(travelEndTime, "yyyy-MM-dd HH:mm:ss", null);
+        endTime = DateTime.ParseExact(travelEndTime, "yyyy-MM-dd HH:mm:ss", null);
         if (endTime < DateTime.Now)
         {
             isTraveling = false;
@@ -40,18 +50,47 @@ public class Travel : MonoBehaviour
             //TODO :: 기념품 획득
             //TODO :: 햄스터 보이도록 설정
             //TODO :: 여행중 마크 제거
+            if(travelIcon != null)
+            {
+                travelIcon.SetActive(false);
+            }
 
             Debug.Log("도착");
         }
 
-        DateTime startTime = DateTime.ParseExact(travelStartTime, "yyyy-MM-dd HH:mm:ss", null);
+        startTime = DateTime.ParseExact(travelStartTime, "yyyy-MM-dd HH:mm:ss", null);
         if (isTraveling == false && startTime < DateTime.Now)
         {
             isTraveling = true;
             //출발
             //TODO :: 햄스터 안보이도록 설정
             //TODO :: 여행중 마크 생성
-
+            if(travelIcon != null)
+            {
+                travelIcon.SetActive(true);
+            }
         }
+    }
+
+    public void OnClickTravelIcon()
+    {
+        GameObject textObj = remainTravelText.transform.parent.gameObject;
+        //TODO :: 남은 시간 표시
+        var remainTime = endTime - DateTime.Now;
+        remainTravelText.text = $"{remainTime.Hours}h {remainTime.Minutes}m left";
+        textObj.SetActive(true);
+        //TODO :: 3초뒤 사라지게 하기
+
+        if(HideCoroutine != null)
+        {
+            StopCoroutine(HideCoroutine);
+        }
+        HideCoroutine = StartCoroutine(C_HideText(textObj));
+    }
+
+    private IEnumerator C_HideText(GameObject obj)
+    {
+        yield return new WaitForSeconds(3f);
+        obj.SetActive(false);
     }
 }
