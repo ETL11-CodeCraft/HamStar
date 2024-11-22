91f6d03bc0e6ccba01cbde1ffb85fa55a88323e6
glwlsl0716 <150993856+glwlsl0716@users.noreply.github.com>
2024-11-22 14:19:35 +0900

    Game Manager 에서 인벤토리 및 배치 정보 로드하고, 쳇바퀴 객체 접근할 수 있도록 수정

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -1,6 +1,70 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static int coin = 0;
+
+    private DataLoader _dataLoader;
+    private InventoryData _inventoryData;
+    private PlacementData _placementData;
+    [SerializeField] ProductData _productData;
+    private HamsterWheel _wheel;
+
+    public HamsterWheel Wheel { 
+        get
+        {
+            return _wheel;
+        }
+    }
+
+    private void Awake()
+    {
+        _dataLoader = new DataLoader();
+    }
+
+    private void Start()
+    {
+        RefreshInventoryData();
+        RefreshPlacementData();
+    }
+
+    public void RefreshInventoryData()
+    {
+        _inventoryData = _dataLoader.Load<InventoryData>();
+        coin = _inventoryData.coin;
+        //_seedCountText.text = $"{GetQuantityForId(0)}";
+        //_goldSeedCountText.text = $"{GetQuantityForId(1)}";
+        //_medicineText.text = $"{GetQuantityForId(2)}";
+    }
+
+    private int GetQuantityForId(int id)
+    {
+        var item = _inventoryData.quantityForProductId.Find((x) => x.productId == id);
+        return item.quantity;
+    }
+
+    public void AddCoin()
+    {
+        GameManager.coin += 10;
+        _inventoryData.coin = GameManager.coin;
+        _dataLoader.Save(_inventoryData);
+        RefreshInventoryData();
+    }
+
+    private void RefreshPlacementData()
+    {
+        // 쳇바퀴 배치 정보 로드
+        _placementData = _dataLoader.Load<PlacementData>();
+
+        _placementData.placements.ForEach(p =>
+        {
+            Product product = GetProduct(p.productId);
+            _wheel = Instantiate(product.prefab, p.position, p.rotation).GetComponent<HamsterWheel>();
+        });
+    }
+
+    private Product GetProduct(int id)
+    {
+        return _productData.list.Find((v) => v.id == id);
+    }
 }
